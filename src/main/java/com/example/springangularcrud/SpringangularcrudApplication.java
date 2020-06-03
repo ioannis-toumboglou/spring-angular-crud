fb3af9031c5a59785dec01b6dd638bae5da03509
toumb <ioannis.toumboglou@gmail.com>
2020-06-03 20:17:50 +0300

    Initial commit

---
+++ src/main/java/com/example/springangularcrud/SpringangularcrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springangularcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringangularcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringangularcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springangularcrud/SpringangularcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springangularcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringangularcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
